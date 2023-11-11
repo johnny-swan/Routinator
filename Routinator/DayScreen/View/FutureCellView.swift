020eb7e6e3d6e09ef751719d2c4673e4622f6b5e
Ivan Lebed <johnny.swan1991@gmail.com>
2023-11-11 18:26:29 +0100

    lost files

---
+++ Routinator/DayScreen/View/FutureCellView.swift
@@ -0,0 +1,17 @@
+//
+//  futureCellViewController.swift
+//  Routinator
+//
+//  Created by Ivan Lebed on 05/11/2023.
+//
+
+import UIKit
+
+class FutureCellView: UITableViewCell {
+
+    @IBOutlet weak var lblCaption: UILabel!
+    @IBOutlet weak var lblStartFrom: UILabel!
+    @IBOutlet weak var lblFinishAt: UILabel!
+    @IBOutlet weak var lblDescription: UILabel!
+    
+}
